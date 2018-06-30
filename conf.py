cd9718746f6a855cd25ca914c276ed536f4f47ad
Anon <anon@example.com>
2018-06-29 19:50:10 -0700

    Latex page numbering

--- conf.py
+++ conf.py
@@ -30,20 +30,20 @@ htmlhelp_basename = 'Digitalsecurityandprivacyworkshopdoc'
 latex_show_urls = 'footnote'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
-    #
     # 'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
-    #
     # 'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
+    'preamble': r'''
 
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+\usepackage{lastpage}
+\usepackage{fancyhdr}
+\pagestyle{fancy}
+\cfoot{\thepage\ / \pageref{LastPage}}
+
+''',
 }
 
 full_title = u'Digital security and privacy workshop'
